47cbd4a231087456436f36b6071eb9d531966df6
subashchandarsiva <subashchandarsiva@gmail.com>
2018-12-06 11:07:26 -0800

    Add files via upload
    play around in js

---
+++ js_works/script.js
@@ -0,0 +1,47 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var lists = document.querySelectorAll("li");
+//console.log(lists);
+var len = lists.length;
+for(var i =0 ;i<len;i++){
+	lists[i].addEventListener("click",function(event){
+	
+		// if(noolen){
+		// 	event.target.classList.toggle(done);
+		// }
+		console.log(event.target);
+		//console.log(noolen);
+		event.target.classList.toggle("done");
+	})
+}
+console.log(ul);
+
+
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
